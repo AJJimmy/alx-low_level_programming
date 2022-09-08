66357a04af69bc6f2ee740c6b0e8078ea780accd
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-08 07:10:20 -0700

    My first c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+
+/*Code to print an output*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts( "Programming is like building a multilingual puzzle" );
 	return(0);
 }
