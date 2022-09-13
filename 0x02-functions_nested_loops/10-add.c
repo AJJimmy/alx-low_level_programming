5c9452195873830ebfa4b991984f09b5cdfc5f6d
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-13 12:05:49 -0700

    My project on c functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Program to add two integers
+ * @a: The first number
+ * @b: The second number
+ *
+ * Return: Added number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
