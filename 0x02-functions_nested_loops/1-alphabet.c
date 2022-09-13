3f00981809816a04ce1633fab0945ebfa93143f7
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-13 05:22:58 -0700

    My project on c functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Program to print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ch;
+	
+	for(ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
