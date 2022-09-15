ae6733e312a96c3db9b4cd30444052570c451099
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-15 07:26:04 -0700

    My alx low level programming project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Program to multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Returns the result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
