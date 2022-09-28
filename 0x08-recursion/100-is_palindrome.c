9f076a132ccc82de6a3e0803269d7d1c0c711047
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-28 15:39:46 -0700

    My alx low level programming project

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - Returns the length of a string
+ * @s: string
+ *
+ * Return: Length of string
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+	len++;
+	len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome
+ * @s: string
+ * @len: length of s
+ * @index: index of string
+ *
+ * Return: 1 or 0
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+	return (1);
+
+	if (s[index] == s[len - index - 1])
+	return (check_palindrome(s, len, index + 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - Function that returns 1 or 0
+ * @s: string
+ *
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
+	return (1);
+
+	return (check_palindrome(s, len, index));
+}
