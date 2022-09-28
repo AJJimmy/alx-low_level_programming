b81730714d2f8eba3a021ebd627a5f22a3692f2a
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-09-28 14:01:45 -0700

    My alx low level programming project

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Function that returns the value of x raised to power y
+ * @x: number
+ * @y: power
+ *
+ *Return: value of x raised to power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+	return (-1);
+
+	else if (y == 0)
+	return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
+}
