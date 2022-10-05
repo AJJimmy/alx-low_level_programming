1ee532b835bd1e9544e14628861f9c0e917f6f9d
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-10-05 02:13:34 -0700

    My alx low level programming project

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that print its name, followed by a new line
+ * @argc: Argument count
+ * argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
