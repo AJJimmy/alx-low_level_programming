8c77fad823e45e85f547ebbad67ca194c732a3e6
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-10-05 02:33:01 -0700

    My alx low level programming project

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all arguments it receives
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
