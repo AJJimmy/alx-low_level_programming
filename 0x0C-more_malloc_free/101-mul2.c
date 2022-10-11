c40ff352a2fdc841eaa58ee898f9c7b7db863b19
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-10-11 02:49:34 -0700

    My alx low level programming project

---
+++ 0x0C-more_malloc_free/101-mul2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_digit - checks if  a string contains a non-digit char
+ * @s: string
+ *
+ * Return: 0 or 1
+ */
+int is_digit(char *s)
+{
+	int i
