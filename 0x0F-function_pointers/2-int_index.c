1ca9cfc602d1d0b25302f8a89c7bfe1e0958543c
Aanuoluwapo Adejimi <aanuoluwaff@gmail.com>
2022-10-12 05:38:45 -0700

    My alx low level programming project

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Function that searches for an integer
+ * @array: array
+ * @size: number of elements in the array
+ * @*cmp: A pointer to the function to be used to compare values
+ *
+ * Return: 0 or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+	return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+	if (cmp(array[index]) != 0)
+	return (index);
+	}
+
+	return (-1);
+}
